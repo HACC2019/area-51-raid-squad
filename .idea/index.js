b09e34a8097cd381e1e200243cc65d1a8c5858a9
KWong41 <46613890+KWong41@users.noreply.github.com>
2019-10-21 16:02:19 -1000

    project update
    updated project so that it is d3js compatible

---
+++ .idea/index.js
@@ -0,0 +1,24 @@
+/*
+Steps/Algorithm on how to make the user interface of this chart
+1.  Import D3 interface into the ReactJS interface:
+
+copy and paste this into index.html directory
+<script src="https://d3js.org/d3.v5.min.js"></script>
+
+2.  Parsing data from the csv file into ReactJS
+
+3.  Import the Data_HACC.csv file into ReactJS
+
+4.  Develop the Algorithm:  X Axis is...?
+
+5.  Develop the Algorithm:  Y Axis is...?
+
+6.  Develop the Algorithm:  Curve
+
+ */
+d3.select();
+d3.selectAll();
+
+d3.select('h1').style('color','red')
+  .attr('class','heading')
+  .text('update');
