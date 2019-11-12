965c05581bd2a12159afb2c1ede44299df3bed2d
Brendt McFeeley <brendtmcfeeley@gmail.com>
2019-11-11 21:14:42 -1000

    consolidate the sidebar

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -24,13 +24,9 @@ const SideNav = () => { return <div id="sidebar-menu">
                                 <Link to="charger_reports" className="waves-effect"><i className="ion ion-md-clipboard"></i><span> Reports </span></Link>
                             </li>
 
-                  <li>
-                    <Link to="/#" className="waves-effect"><i className="ion ion-md-pin"></i><span> Maps <span className="float-right menu-arrow"><i className="mdi mdi-chevron-right"></i></span> </span></Link>
-                    <ul className="submenu">
-                      <li><Link to="maps-google"> Google Map</Link></li>
-                      <li><Link to="maps-vector"> Vector Map</Link></li>
-                    </ul>
-                  </li>
+                            <li>
+                                <Link to="maps" className="waves-effect"><i className="ion ion-md-pin"></i><span> Map </span></Link>
+                            </li>
                 </ul>
     </div>
 }
